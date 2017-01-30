30c2be3b48f962eadb4cf07826abbdd62e330def
Dirk Eddelbuettel <edd@debian.org>
2017-01-30 14:35:35 +0000

    version 0.1.2

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 ## run from root directory against source (installed=FALSE) or installed directory
 .rawDataToSavedData <- function(installed=FALSE) {
     if (installed) {
-        alldata <- read_csv(system.file("rawdata", "ncis_bystate_bymonth_bytype.csv", package="gunsales"), na = "#N/A")
-        poptotal <- read_csv(system.file("rawdata", "population.csv", package="gunsales"))
+        alldata <- read.csv(system.file("rawdata", "ncis_bystate_bymonth_bytype.csv", package="gunsales"), na = "#N/A", stringsAsFactors=FALSE)
+        poptotal <- read.csv(system.file("rawdata", "population.csv", package="gunsales"), stringsAsFactors=FALSE)
     } else {                          
-        alldata <- read_csv("inst/rawdata/ncis_bystate_bymonth_bytype.csv", na = "#N/A")
-        poptotal <- read_csv("inst/rawdata/population.csv")
+        alldata <- read.csv("inst/rawdata/ncis_bystate_bymonth_bytype.csv", na = "#N/A", stringsAsFactors=FALSE)
+        poptotal <- read.csv("inst/rawdata/population.csv", stringsAsFactors=FALSE)
     }
     save(alldata, file="data/alldata.RData", compress="xz", compression_level=9)    
     save(poptotal, file="data/poptotal.RData", compress="xz", compression_level=9)
@@ -18,7 +18,7 @@
 #' @docType data
 #' @name alldata
 #' @format A \sQuote{tbl_df} and data frame object with 11480 observation of 34 variables
-#' @source \url{https://www.fbi.gov/about-us/cjis/nics/reports/nics_firearm_checks_-_month_year_by_state_type.pdf}
+#' @source \url{https://www.fbi.gov/file-repository/nics_firearm_checks_-_month_year_by_state_type.pdf}
 #' @seealso The GitHub repository created by Buzzfeed containing a similar data set
 #' at \url{https://github.com/BuzzFeedNews/nics-firearm-background-checks/}
 NULL
